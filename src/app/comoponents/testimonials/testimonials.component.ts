533bda76208f0b96d8298a3d6241540a565f4f1b
mohamed857 <mohamed857>
2024-08-03 18:33:51 +0300

    update

--- src/app/comoponents/testimonials/testimonials.component.ts
+++ src/app/comoponents/testimonials/testimonials.component.ts
@@ -8,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrl: './testimonials.component.scss'
 })
 export class TestimonialsComponent {
-
+  testimonials = [
+    { id: 1, name: 'Ibrahim', rate: 5, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/ibrahim.jpg' },
+    { id: 2, name: 'Abdelrhman', rate: 4, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/Abdelrhman.jpg' },
+    { id: 3, name: 'Abdelrhman Mamdouh', rate: 5, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/mamdouh.jpg' },
+    // { id: , name:'', description:'',img:''},
+  ]
+  arryOfNumber =()=>{
+    
+  }
 }
