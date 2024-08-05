6a92a1380613f99f463d06568a69ce3744d2ef58
mohamed857 <mohamed857>
2024-08-06 01:44:02 +0300

    update

--- src/app/comoponents/testimonials/testimonials.component.ts
+++ src/app/comoponents/testimonials/testimonials.component.ts
@@ -11,7 +11,7 @@ export class TestimonialsComponent {
   testimonials = [
     { id: 1, name: 'ghareb', rate: 5, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/ghareb.png' },
     { id: 2, name: 'Zain', rate: 5, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/zain.jpg' },
-    { id: 3, name: 'walid', rate: 3, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/walid.jpg' },
+    { id: 3, name: 'walid', rate: 3, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit orem ipsum dolor sit amet, consectetur adipisicing elit orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/walid.jpg' },
     { id: 4, name: 'Ibrahim', rate: 5, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/ibrahim.jpg' },
     { id: 5, name: 'Abdelrhman', rate: 4, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/Abdelrhman.jpg' },
     { id: 6, name: 'Abdelrhman Mamdouh', rate: 2, description: 'orem ipsum dolor sit amet, consectetur adipisicing elit.', img: 'assets/images/clients/mamdouh.jpg' },
